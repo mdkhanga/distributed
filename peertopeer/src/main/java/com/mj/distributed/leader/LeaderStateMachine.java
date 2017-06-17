4b41b1fb3bf90876e4d01f07fb0e3caca02e3bff
Manoj Khangaonkar <manoj.khangaonkar@veeva.com>
2017-06-17 10:17:15 -0700

    start leadership code

---
+++ peertopeer/src/main/java/com/mj/distributed/leader/LeaderStateMachine.java
@@ -0,0 +1,31 @@
+package com.mj.distributed.leader;
+
+
+public class LeaderStateMachine {
+
+    public enum MemberState {
+        LEADER,
+        FOLLOWER
+    }
+
+    private MemberState state ;
+
+    public LeaderStateMachine() {
+        state = state.FOLLOWER ;
+    }
+
+    public void start() {
+
+    }
+
+    /**
+     *  method called tp pass data that would change the state of this machine
+     *
+     *
+     */
+    public void update(/* params */) {
+
+
+
+    }
+}
